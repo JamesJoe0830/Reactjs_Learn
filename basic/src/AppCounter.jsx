c3a86d7d5b5508c0fe86b0a8c55be255b2c7bf8c
joejames0830 <kyeun950830@gmail.com>
2023-04-15 23:36:43 +0900

    feat: useEffect

--- basic/src/AppCounter.jsx
+++ basic/src/AppCounter.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 import Counter from "./components/Counter";
 
-
 export default function AppCounter() {
-    return (
-        <div>
-            <Counter/>
-        </div>
-    );
+  const [count, setCount] = useState(0);
+  const handleClick = () => setCount((prev) => prev + 1);
+// 자식 component에 props로 넘겨줄 total
+  return (
+    <div>
+      <div className="banner">
+        Total Count = {count} {count > 10 ? "🔥" : "❄️"}
+      </div>
+      {/* 이부분 다시보기 */}
+      <Counter total={count}  onClick={handleClick}/> 
+      <Counter total={count}  onClick={handleClick}/>
+    
+    </div>
+  );
 }
-
--- basic/src/components/Counter.jsx
+++ basic/src/components/Counter.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { useState } from 'react';
-export default function Counter() {
-    const [count, setCount] = useState(0);
-    return (
-        <div className='counter'>
-            <span className='number'>{count}</span>
-            <button className = 'button' onClick = {()=>{
-                setCount(count + 1 ); 
-                //왜 setCount = count + 1 하면 안되는가 ?
-            }}>Add + </button>
-            
-        </div>
-    );              
+import React from "react";
+import { useState } from "react";
+export default function Counter({ total, onClick }) {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="counter">
+      <p className="number">
+        {count} <span>/{total}</span>
+      </p>
+      <button
+        className="button"
+        onClick={() => {
+          setCount((prev) => prev + 1);
+          onClick();
+          // setCount(count + 1 );
+          //왜 setCount = count + 1 하면 안되는가 ?
+        }}
+      >
+        Add +
+      </button>
+    </div>
+  );
 }
-
